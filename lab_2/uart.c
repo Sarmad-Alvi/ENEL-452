02e6b8c5385cc471ad0aa32aad5f512d9866ae88
Sarmad Alvi <sarmadwadood@hotmail.com>
2023-10-05 19:40:54 -0600

    Added uart2 termination function

--- lab_2/uart.c
+++ lab_2/uart.c
@@ -42,6 +42,19 @@ void uart2_init(void)
 	USART2->CR1 |= USART_CR1_UE;
 }
 
+void uart2_terminate(void)
+{
+	RCC->APB1ENR &=  ~RCC_APB1ENR_USART2EN ;
+	RCC->APB2ENR &= ~(RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN);
+	//GPIOA->CRH &= 0xFFFFFFFB;
+	GPIOA->CRL &= ~(GPIO_CRL_MODE2 | GPIO_CRL_CNF2_1);
+	
+	USART2->CR1 &= ~USART_CR1_UE;
+	USART2->BRR = 0x0;
+	USART2->CR1 &= ~(USART_CR1_RE | USART_CR1_TE);
+	USART2->CR1 &= ~(USART_CR1_UE);
+}
+
 void uart2_send(uint16_t ascii)
 {
 	while (!(USART2->SR & USART_SR_TXE))
