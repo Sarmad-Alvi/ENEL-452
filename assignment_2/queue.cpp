ca2af7d58ab5c1de98c4fb818786750c236d4d36
saalvi <sarmadwadood@hotmail.com>
2023-10-18 21:15:58 -0600

    Finished assignment 2

---
+++ assignment_2/queue.cpp
@@ -0,0 +1,130 @@
+/**
+   Project: Implementation of a Queue in C++.
+   Programmer: Karim Naqvi
+   Course: enel452
+*/
+
+#include "queue.h"
+#include <iostream>
+#include <cstdlib>              // for exit
+
+using namespace std;
+
+Queue::Queue()
+{
+    head = 0;
+    tail = 0;
+    nelements = 0;
+    verbose = false;
+}
+
+Queue::~Queue()
+{
+    for (QElement* qe = head; qe != 0;)
+    {
+	QElement* temp = qe;
+	qe = qe->next;
+	delete(temp);
+    }
+}
+
+void Queue::remove(Data* d)
+{
+    if (size() > 0)
+    {
+        QElement* qe = head;
+        head = head->next;
+        nelements--;
+        *d = qe->data;
+	delete qe;
+    }
+    else
+    {
+        cerr << "Error: Queue is empty.\n";
+        exit(1);
+    }
+}
+
+void Queue::insert(Data d)
+{
+    if (verbose) std::cout << "insert(d)\n";
+    QElement* el = new QElement(d);
+    if (size() > 0)
+    {
+        tail->next = el;
+    }
+    else
+    {
+        head = el;
+    }
+    tail = el;
+    nelements++;
+}
+
+void Queue::insert(Data d, unsigned position)
+{
+    if (position <= size())
+    {
+        QElement* el = new QElement(d);
+
+        if (position == 0 && size() > 0)
+        {
+            el->next = head;
+            head = el;
+            nelements++;
+        }
+        else if (position == size())
+        {
+            insert(d);
+        }
+        else
+        {
+            QElement* index = head;
+            for (unsigned i = 0; i < (position - 1); i++)
+            {
+                index = index->next;
+            }
+            el->next = index->next;
+            index->next = el;
+            nelements++;
+        }
+
+    }
+    else
+    {
+        errno = 3;
+        cout << "insert: range error." << endl;
+        exit(3);
+    }
+}
+
+bool Queue::search(Data otherData) const
+{
+    QElement* insideEl = head;
+    for (int i = 0; i < nelements; i++)
+    {
+        if (insideEl->data.equals(otherData))
+            return true;
+        insideEl = insideEl->next;
+    }
+    return false;
+}
+
+void Queue::print() const
+{
+    QElement* qe = head;
+    if (size() > 0)
+    {
+        for (unsigned i = 0; i < size(); i++)
+        {
+            cout << i << ":(" << qe->data.x << "," << qe->data.y << ") ";
+            qe = qe->next;
+        }
+    }
+    cout << "\n";
+}
+
+unsigned Queue::size() const
+{
+    return nelements;
+}
